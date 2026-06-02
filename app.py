9d70602e438007f4b9a495ed910d9c1f28a7b2c8
ayush-singh110 <ayushsingh200411@gmail.com>
2026-06-02 18:45:52 +0530

    Embedding model changes

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 from langchain_groq import ChatGroq
 from langchain_community.document_loaders import PyMuPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_community.embeddings import OllamaEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_classic.chains import create_retrieval_chain
@@ -12,12 +12,13 @@ from langchain_classic.chains.combine_documents import create_stuff_documents_ch
 
 load_dotenv()
 os.environ["GROQ_API_KEY"]=os.getenv("GROQ_API_KEY")
+os.environ["HUGGINGFACEHUB_API_TOKEN"]=os.getenv("HUGGINGFACEHUB_API_TOKEN")
 
 llm=ChatGroq(model="llama-3.3-70b-versatile")
 docs=PyMuPDFLoader("document\\toaz.info-india-national-building-code-nbc-2016-vol-1pdf-pr_26cbd1ad8b437409f28176f6fefcbb10.pdf").load()
 text_splitter=RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=200)
 splitter=text_splitter.split_documents(docs)
-embeddings=OllamaEmbeddings(model='nomic-embed-text-v2-moe')
+embeddings=HuggingFaceEmbeddings(model_name="BAAI/bge-small-en-v1.5")
 vec_store=FAISS.from_documents(splitter,embeddings)
 retriever=vec_store.as_retriever()
 template="""
